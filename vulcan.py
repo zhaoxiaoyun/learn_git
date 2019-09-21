91ddc22221cd68260697bac9619aba0db7e35a54
ZXY <zhao-xiao-yun@126.com>
2019-09-21 14:06:35 +0800

    add feature-vulcan

---
+++ vulcan.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"WebApp-d_01 开发环境" # 第一个字符，文档注释
+
+__author__ = 'ZXY' #特殊变量 标示作者。以上为标准文件模板
+
+#测试git 新建feature
+input = input('input your name:')
+print('Hello, %s' % input)
